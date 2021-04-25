65c44cc2ebeea5d4c134519f2f873a251eaa458b
CRPNTRPINK <ultrololl@gmail.com>
2021-04-25 20:47:57 +0300

    update models, services, templates

--- src/main/java/com/example/courseWork/dao/UserDAO.java
+++
@@ -1,9 +0,0 @@
-package com.example.courseWork.dao;
-
-import com.example.courseWork.models.User;
-
-public interface UserDAO {
-
-    public void save(User user);
-    public User findByUsername(String username);
-}
